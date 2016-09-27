7d2b14da8c0992aad25da687636dc690f8ff2569
cokeito <coke@mta.cl>
2016-09-26 21:25:56 -0300

    2 done

--- 02/2.rb
+++ 02/2.rb
@@ -21,18 +21,41 @@ a.shift
 pp a
 
 #3
-a = [1,2,3,9,1,4,5,2,3,6,6,2]
+a = [1,2,3,4,5,6,7,8] #,1,4,5,2,3,6,6,2]
 pp "s: #{a}"
 c = a.length
 pp c
 
 if c % 2 == 0 
-	half = c / 2
+	half = (c / 2) -1
 else
-	half = (c-1) / 2
+	half = (c) / 2
 end
 
-a.delete_at(half-1)
+a.delete_at(half)
 pp "e: #{a}" 
+return
 
-#4
+#4 Borrar el último elemento mientras ese número sea distinto a 1
+puts "4-------"
+a = [1,2,3,9,1,4,5,2,3,6,6]
+pp a
+last = a[-1]
+a.pop if last != 1
+pp a
+
+# 5.- Utilizando un arrego vacío auxiliar y operaciones de push and pop invertir el orden de los elementos en el arreglo
+puts "5-------"
+a = [1,2,3,9,1,4,5,2,3,6,6]
+b = []
+
+c = a.length
+
+a.each_with_index do |val, i|
+	b.push( a[c-1] )
+	c = c -1
+	
+end
+
+pp b
+
